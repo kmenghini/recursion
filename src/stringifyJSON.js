e7dcd8d0598ca560f2698f7419c955966778e2eb
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2017-11-13 01:49:41 -0800

    Add cases for data types No recursive logic added yet

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,23 +2,32 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+//var stringOutput = '';
 
 var stringifyJSON = function(obj) {
   // move from outside data type to inside
   //update input for next iteration
   //add strigified part to output
   //if it's a number
-  	//number.toString
-  //if it's null
-  	//'null'
-  //if boolean
-  	//bool.toString
-  //if string
-  	//return string
-  //if array
-  	//add [] around 
-  	//and loop through each element
-  //if object
-  	//add {} around
-  	//and loop through each key				 	
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+  	return obj.toString();
+  } else if (typeof obj === 'string') {
+  	return obj;
+  } else if (obj === null) {
+  	return 'null';
+  } else if (Array.isArray(obj)) {
+  	return '[' + obj + ']';
+  	//loop through each element into Stringify JSON
+  } else if (typeof obj === 'object') {
+  	return '{' + obj + '}';
+  	//loop
+  } else if (typeof obj === 'function' || typeof obj === 'undefined') {
+  	return;
+  }
+  
+  //also cases for function, undefined, and symbol
+  // while(){
+  // 	stringifyJSON(obj);		
+  // }		
+  //return string;	 	
 };
