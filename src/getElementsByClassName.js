41ca5fefbe2166274833c75eb510279901d8bd50
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2017-11-13 14:04:34 -0800

    Add high level strategy

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,11 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  //initalize array to add to
+  var elementsInClass = [];
+  //initialize current node to document.body
+  //loop into each node
+  //if the classList inclues the className
+  	//add the element to the array
+  	//go into next childNode
 };
